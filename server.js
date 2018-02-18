4d0da580560161b5e8ebde26e5666c60414de320
Glitch (puppeteer-test) <none>
2018-02-18 19:04:58 +0000

    ☕️⛪️ Checkpoint server.js:306691/261 README.md:306691/96

--- server.js
+++ server.js
@@ -15,26 +15,12 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   puppeteer.launch()
-    .then(response.status(200).send('Puppeteer worked'))
-    .catch(error => response.status(500).send(error.message))
+    .then(browser => response.status(200).type('html').send(`<p>Puppeteer worked: ${browser}</p>`))
+    .catch(error => {
+      const message = error.message.split('\n').join('</br>')
+      response.status(500).type('html').send(`<code>${message}</code>`)
+    })
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
