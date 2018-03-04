85116d8a11a5e9c59d6d71ac3b51932a9217ecc4
Glitch (puppeteer-example) <none>
2018-03-04 23:05:02 +0000

    🙇🎪 Checkpoint server.js:111231/397 package.json:111231/10

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express'),
     app = express(),
     puppeteer = require('puppeteer');
-
+ 
 app.get("/", async (request, response) => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    executablePath: 'node_modules/puppeteer/.local-chromium/linux-536395/chrome-linux/chrome'
-  });
-  const page = await browser.newPage();
-  await page.goto('https://www.nasdaq.com/symbol/goog');
-  await page.screenshot({path: 'goog.png'});
-  await browser.close();
-  response.sendFile('goog.png');
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.developers.google.com/web/tools/puppeteer');
+    await page.screenshot({path: 'puppeteer.png'});
+    await browser.close();
+    response.sendFile('puppeteer.png');
+  } catch (error) {
+    console.log(error); // test
+  }
 });
 
 var listener = app.listen(process.env.PORT, function () {
