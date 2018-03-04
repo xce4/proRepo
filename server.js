8070de7e715eb6c17515b0917209e779ec838c28
Glitch (puppeteer-example) <none>
2018-03-04 22:44:23 +0000

    🌅🚗 Checkpoint server.js:111231/382 package.json:111231/261

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express'),
     app = express(),
     puppeteer = require('puppeteer');
 
-app.get("/", (request, response) => {
-  puppeteer.launch({args: ['--no-sandbox']})
-    .then(browser => response.status(200).type('html').send(`<p>Puppeteer worked: ${browser}</p>`))
-    .catch(error => {
-      const message = error.message.split('\n').join('</br>')
-      response.status(500).type('html').send(`<code>${message}</code>`)
-    })
+app.get("/", async (request, response) => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://www.nasdaq.com/symbol/goog');
+  await page.screenshot({path: 'goog.png'});
+  await browser.close();
+  response.sendFile('goog.png');
 });
 
 var listener = app.listen(process.env.PORT, function () {
