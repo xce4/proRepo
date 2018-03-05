07d453f2f96830c18a60db4b006931a55c04b6ad
Glitch (pickle-clutch) <none>
2018-03-05 03:30:17 +0000

    ⛷🏩 Checkpoint server.js:11/43 package.json:11/11

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ app.get("/", async (request, response) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox',
-             '--remote-debugging-address=0.0.0.0',
-             '--remote-debugging-port=80'],
-      headless: true
+             //'--remote-debugging-address=0.0.0.0',
+             '--remote-debugging-port=3000'],
+     // headless: true
      // setRequestInterceptionEnabled: true,
       //ignoreHTTPSErrors: true
     });
     console.log('1')
     const page = await browser.newPage();
     console.log('2')
-    await page.goto('https://www.glitch.com/', {timeout: 0});
+    await page.goto('https://www.glitch.com/');
     console.log('3')
     await page.screenshot({path: __dirname+'/public/puppeteer.png'});
     console.log('4')
